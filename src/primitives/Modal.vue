77ec6ce7167d6c81654641e89c56c73d8d0678a8
anthocodes99 <anthocodes99@users.noreply.github.com>
2022-02-12 15:12:09 +0800

    Added Modal

---
+++ src/primitives/Modal.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+interface Props {
+    id: string
+    color: string
+    title: string
+    desc: string
+    btnColor: string
+    btnText: string
+}
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content bg-dark">
+            <div class="modal-header">
+                <h5 class="modal-title" :class="color" :id="id">
+                    {{ title }}
+                </h5>
+                <button
+                    type="button"
+                    class="btn-close"
+                    data-bs-dismiss="modal"
+                    aria-label="Close"
+                ></button>
+            </div>
+            <div class="modal-body text-light">
+                <slot></slot>
+            </div>
+            <div class="modal-footer">
+                <button
+                    type="button"
+                    class="btn btn-secondary"
+                    data-bs-dismiss="modal"
+                >
+                    Cancel
+                </button>
+                <button
+                    @click="this.$emit('btnClick')"
+                    type="button"
+                    class="btn btn-danger"
+                    :class="btnColor"
+                    data-bs-dismiss="modal"
+                >
+                    {{ btnText }}
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
