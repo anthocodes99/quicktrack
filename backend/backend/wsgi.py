c315f3a0f39823065197993cccb36edb90811747
anthocodes99 <anthocodes99@users.noreply.github.com>
2022-02-14 00:05:53 +0800

    build: gunicorn now runs on development on default
    Assuming no enviroment variable given.

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -12,6 +12,6 @@ import logging
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings.development')
 
 application = get_wsgi_application()
