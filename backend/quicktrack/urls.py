1c9525e92ff740e2678279a6ed714d60df7b2e23
anthocodes99 <anthocodes99@users.noreply.github.com>
2022-02-14 00:14:04 +0800

    refactor: disabled some path codes for quicktrack
    My current assumption is that I wont be using media url.
    Staticfiles are served in the base url(root url).

--- backend/quicktrack/urls.py
+++ backend/quicktrack/urls.py
@@ -9,7 +9,13 @@ from quicktrack import views
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html')),
-    path('service-worker.js', (TemplateView.as_view(template_name="service-worker.js", content_type='application/javascript', )), name='sw.js'),
+    #     path('registerSW.js', (TemplateView.as_view(template_name="registerSW.js",
+    #     content_type='application/javascript', )), name='sw.js'),
+    #     path('sw.js', (TemplateView.as_view(template_name="sw.js",
+    #     content_type='application/javascript', )), name='sw.js'),
+
+    #     path('manifest.webmanifest', (TemplateView.as_view(template_name="manifest.webmanifest",
+    # content_type='application/json', )), name='manifest'),
 
     path('api/admin/curruser', views.GetCurrUser.as_view()),
     path('api/accounts', views.AccountList.as_view()),
@@ -17,7 +23,7 @@ urlpatterns = [
     path('api/accounts/<int:pk>/merge', views.AccountMerge.as_view()),
     path('api/accounts/<int:pk>/sales', views.AccountSale.as_view()),
     path('api/accounts/<int:pk>/history', views.ListAccountHistory.as_view()),
-    
+
     # fallback path
     re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
 ]
