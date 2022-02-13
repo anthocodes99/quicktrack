27c65b382b001ecc5a17bb0605a12f4b256fb1e1
anthocodes99 <anthocodes99@users.noreply.github.com>
2022-02-14 00:59:16 +0800

    build: added `v3-quicktrack` to `ALLOWED_HOSTS`

--- backend/backend/settings/production.py
+++ backend/backend/settings/production.py
@@ -1,9 +1,11 @@
+import dj_database_url
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['', '']
+ALLOWED_HOSTS = ['',
+                 '', '']
 
 # SSL / CSRF / CSP
 
@@ -17,7 +19,6 @@ DATABASES = {
     }
 }
 
-import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 
