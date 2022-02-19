279b6065713e72fcf4d29f19bebe4a81ac3ac5ad
anthocodes99 <anthocodes99@users.noreply.github.com>
2022-02-19 10:07:50 +0800

    fix(frontend): ReloadPrompt.. not prompting
    I think this is because the ref values are only updated after the checks.

--- src/components/ReloadPrompt.vue
+++ src/components/ReloadPrompt.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { useRegisterSW } from 'virtual:pwa-register/vue'
+import { watch, watchEffect } from 'vue'
 import { useToast } from '../composables/toast'
 
 const toast = useToast()
@@ -13,26 +14,31 @@ const close = async () => {
 
 console.log({ offlineReady }, { needRefresh })
 
-if (offlineReady.value) {
-    toast.info('App Ready', 'App is ready to work offline')
-}
-if (needRefresh.value) {
-    const buttons = [
-        {
-            btnText: 'Update',
-            btnStatus: 'btn-primary',
-            btnCb: updateServiceWorker,
-        },
-    ]
-    toast.info(
-        'New Update Available',
-        'Click on Reload button to Update.',
-        buttons,
-        20000
-    )
-}
+watch(offlineReady, (curr, prev) => {
+    if (curr) toast.info('App Ready', 'App is ready to work offline')
+})
+
+watch(needRefresh, (curr, prev) => {
+    if (curr) {
+        const buttons = [
+            {
+                btnText: 'Update',
+                btnStatus: 'btn-primary',
+                btnCb: updateServiceWorker,
+            },
+        ]
+        toast.info(
+            'New Update Available',
+            'Click on Reload button to Update.',
+            buttons,
+            20000
+        )
+    }
+})
 </script>
 
-<template></template>
+<template>
+    <div></div>
+</template>
 
 <style></style>
