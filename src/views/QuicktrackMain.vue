468c83a25b02e6ce07ef6bbd2b1a53e44f86b2ca
anthocodes99 <anthocodes99@users.noreply.github.com>
2022-02-24 09:44:26 +0800

    feat(frontend): added spinner for QuicktrackMain

--- src/views/QuicktrackMain.vue
+++ src/views/QuicktrackMain.vue
@@ -36,7 +36,15 @@ onMounted(async () => {
 <template>
     <TheNavbar />
     <div v-if="!isInitialized">
-        <h1>Loading...</h1>
+        <div class="d-flex justify-content-center">
+            <div
+                class="spinner-border text-primary mt-5"
+                style="width: 3rem; height: 3rem"
+                role="status"
+            >
+                <span class="visually-hidden">Loading...</span>
+            </div>
+        </div>
     </div>
     <div v-else>
         <HomeUserList :accounts="accounts" />
