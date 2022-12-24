a9ab8e8d544d02528496329076acb1bf40e89402
anthocodes99 <anthocodes99@users.noreply.github.com>
2022-12-25 01:01:28 +0800

    build(frontend): :heavy_plus_sign: Added tailwindcss

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import '@fortawesome/fontawesome-free/css/all.css'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { createPinia } from 'pinia'
+import './style.css'
 const pinia = createPinia()
 
 createApp(App).use(pinia).use(router).mount('#app')
