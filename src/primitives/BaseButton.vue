cb23f43e581cec6c6e965de26b6fdfd4325495d2
anthocodes99 <anthocodes99@users.noreply.github.com>
2023-01-23 10:15:40 +0800

    feat(frontend): :sparkles: Added BaseButton and BaseInput

---
+++ src/primitives/BaseButton.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+    <button
+        type="button"
+        class="text-white bg-[#E94F37] border-1 w-full rounded-md py-2 transition ease-in-out hover:bg-[#e45742] focus:border-[#44bba4]"
+    >
+        <slot></slot>
+    </button>
+</template>
+
+<style scoped></style>
