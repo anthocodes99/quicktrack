4efe08415378fbb6eeb214d637ecc514423c7f3e
anthocodes99 <anthocodes99@users.noreply.github.com>
2023-01-23 10:12:08 +0800

    refactor(frontend): :heavy_minus_sign: BREAKING CHANGE: Removed Bootstrap
    Project will be moving towards fully utilizing tailwindcss.

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import router from './router/index'
 import App from './App.vue'
 
 import '@fortawesome/fontawesome-free/css/all.css'
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
+// import 'bootstrap'
+// import 'bootstrap/dist/css/bootstrap.min.css'
 import { createPinia } from 'pinia'
 import './style.css'
 const pinia = createPinia()
