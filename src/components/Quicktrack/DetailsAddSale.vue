1839415fc6f26a7b9ff92f619e1c116700f227c1
anthocodes99 <anthocodes99@users.noreply.github.com>
2023-01-24 15:11:01 +0800

    feat(frontend): :construction: Implemented Quicktrack Detail View

---
+++ src/components/Quicktrack/DetailsAddSale.vue
@@ -0,0 +1,87 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import BaseButton from '../../primitives/BaseButton.vue'
+
+interface Props {
+    products: string[]
+}
+
+const props = defineProps<Props>()
+const selectedProduct = ref('')
+</script>
+
+<template>
+    <form>
+        <label class="text-gray-400 text-lg" for="product">Product </label>
+        <fieldset>
+            <legend class="sr-only">Product</legend>
+            <div class="grid grid-cols-2 gap-4 pt-2">
+                <template v-for="product in products" :key="product">
+                    <label
+                        class="group relative border rounded-md py-3 px-4 flex items-center justify-center text-sm font-medium uppercase hover:bg-[#1b1d1e] sm:flex-1 active:ring-2 active:ring-[#E94F37] bg-[#181a1b] shadow-sm text-[#d6d3cd] cursor-pointer checked:border-[#E94F37] checked:border-2"
+                    >
+                        <input
+                            type="radio"
+                            name="product"
+                            class="sr-only"
+                            :value="product"
+                            aria-labelledby="size-choice-0-label"
+                            v-model="selectedProduct"
+                        />
+                        <span id="size-choice-0-label">{{ product }}</span>
+                        <span
+                            class="border-2"
+                            :class="[
+                                selectedProduct === product
+                                    ? 'border-[#E94F37]'
+                                    : 'border-transparent',
+                                'pointer-events-none absolute -inset-px rounded-md',
+                            ]"
+                            aria-hidden="true"
+                        />
+                    </label>
+                </template>
+            </div>
+        </fieldset>
+        <div class="pt-4">
+            <label class="text-gray-400 text-lg" for="date">Date</label>
+            <input
+                class="block mt-2 p-2 rounded w-48"
+                type="date"
+                name="date"
+                :value="new Date().toISOString().split('T')[0]"
+                id=""
+            />
+        </div>
+        <div class="pt-4">
+            <label class="text-gray-400 text-lg" for="unitprice"
+                >Unit Price</label
+            >
+            <input
+                class="block mt-2 p-2 rounded w-48"
+                type="number"
+                name="unitprice"
+                step="0.0001"
+                value="0.0000"
+                id=""
+            />
+        </div>
+        <div class="pt-4">
+            <label class="text-gray-400 text-lg" for="Quantity">Quantity</label>
+            <input
+                class="block mt-2 p-2 rounded w-48"
+                type="number"
+                name="quantity"
+                step="1"
+                value="1"
+                id=""
+            />
+        </div>
+
+        <BaseButton class="mt-8 font-semibold" type="submit"
+            >Add Sale</BaseButton
+        >
+    </form>
+</template>
+
+<style scoped></style>
--- src/views/QuicktrackDetails.vue
+++ src/views/QuicktrackDetails.vue
@@ -3,6 +3,8 @@ import axios from 'axios'
 import { computed, onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
 
+import CompositeNav from '../components/CompositeNav.vue'
+
 import TheNavbar from '../components/TheNavbar.vue'
 import AnthoButtons from '../components/Quicktrack/AnthoButtons.vue'
 import DetailsSubmitPayment from '../components/Quicktrack/DetailsSubmitPayment.vue'
@@ -16,6 +18,7 @@ import { useMonthdataStore } from '../store/monthdata'
 import { destructureAxios } from '../utils/utils'
 
 import { Account } from '../models/quicktrack'
+import DetailsAddSale from '../components/Quicktrack/DetailsAddSale.vue'
 import DetailsCustomSale from '../components/Quicktrack/DetailsCustomSale.vue'
 import DetailsAddHutang from '../components/Quicktrack/DetailsAddHutang.vue'
 import DetailRecentSales from '../components/Quicktrack/DetailRecentSales.vue'
@@ -87,26 +90,12 @@ onMounted(async () => {
     isInitialized.value = true
 })
 
-const createSale = async function (product, unitPrice, quantity, date = null) {
-    if (monthdata.monthdatas.length === 0)
-        return toast.error(
-            'Did not create Sale!',
-            'You have yet to initialize Runcitworks!'
-        )
-    let dateData: string | null = date
-    if (date == null) {
-        const date = new Date()
-        const [month, day, year] = [
-            // for the love of god getMonth() starts from 0
-            date.getMonth() + 1,
-            date.getDate(),
-            date.getFullYear(),
-        ]
-        dateData = `${year}-${month}-${day}`
-    }
+const createSale = async function (event) {
+    const formData = Object.fromEntries(new FormData(event.target))
+    const { date, product, unitprice: unitPrice, quantity } = formData
     const data = {
         monthdata: monthdataId.value,
-        date: dateData,
+        date: date,
         product: product,
         unit_price: unitPrice,
         quantity: quantity,
@@ -169,70 +158,86 @@ const submitPayment = async function (event) {
 </script>
 
 <template>
-    <TheNavbar />
-    <div class="container-main container-sm col-md-9 col-lg-7 border border-3">
-        <template v-if="!isInitialized">
-            <h1>Loading...</h1>
-        </template>
-        <template v-else-if="isError">
-            <h1>An error occured.</h1>
-            <p>It seems like we hic'd.</p>
-        </template>
-        <template v-else>
-            <h1 class="text-center">{{ account!.username }}</h1>
-            <h2 class="text-center">Hutang : RM {{ account!.hutang }}</h2>
-            <template v-if="store.username == 'antho'">
-                <AnthoButtons @makeSale="createSale" />
-            </template>
-            <div class="container">
-                <label class="form-label fs-4" for="monthdata-selector"
-                    >Monthdata</label
-                >
-                <div v-if="monthdatas.length === 0">
-                    <p class="lead">
-                        You haven't initialize Runcitworks yet.
-                        <router-link :to="{ name: 'Runcitworks' }">
-                            Head there?
-                        </router-link>
-                    </p>
-                </div>
-                <select
-                    v-else
-                    v-model="monthdataId"
-                    class="form-select"
-                    id="monthdata-selector"
-                >
-                    <option
-                        selected
-                        v-for="monthdata in monthdatas"
-                        :key="monthdata.id"
-                        :value="monthdata.id"
+    <CompositeNav>
+        <div class="w-full sm:max-w-md sm:mx-auto md:mx-0">
+            <template v-if="!isInitialized">
+                <div class="mt-32 flex justify-center" role="status">
+                    <svg
+                        aria-hidden="true"
+                        class="w-16 h-16 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                        viewBox="0 0 100 101"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
                     >
-                        {{ monthdata.month }}
-                    </option>
-                </select>
-                <DetailsCustomSale
-                    :products="currentMonthdataProducts"
-                    @submitSale="
-                        createSale(
-                            $event.product,
-                            $event.unitPrice,
-                            $event.quantity,
-                            $event.date
-                        )
-                    "
-                />
-                <DetailsAddHutang />
-                <DetailsSubmitPayment @submitPayment="submitPayment" />
-                <DetailRecentSales :account="account!" />
-            </div>
-        </template>
-    </div>
+                        <path
+                            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                            fill="currentColor"
+                        />
+                        <path
+                            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                            fill="currentFill"
+                        />
+                    </svg>
+                    <span class="sr-only">Loading...</span>
+                </div>
+            </template>
+            <template v-else-if="isError">
+                <!-- TODO: Error Page -->
+                <h1>An error occured.</h1>
+                <p>It seems like we hic'd.</p>
+            </template>
+            <template v-else>
+                <div class="p-4">
+                    <!-- Account Name & Hutang -->
+                    <h1 class="text-4xl">{{ account!.username }}</h1>
+                    <h2 class="text-2xl pt-2">
+                        RM {{ account!.hutang.toFixed(2) }}
+                    </h2>
+
+                    <!-- Add Sale Form -->
+                    <DetailsAddSale
+                        class="pt-8"
+                        :products="currentMonthdataProducts"
+                        @submit.prevent="createSale"
+                    />
+
+                    <!-- Runcitworks Monthdata Selector -->
+                    <div class="container">
+                        <label class="form-label fs-4" for="monthdata-selector"
+                            >Monthdata</label
+                        >
+                        <div v-if="monthdatas.length === 0">
+                            <p class="lead">
+                                You haven't initialize Runcitworks yet.
+                                <router-link :to="{ name: 'Runcitworks' }">
+                                    Head there?
+                                </router-link>
+                            </p>
+                        </div>
+                        <select
+                            v-else
+                            v-model="monthdataId"
+                            class="form-select"
+                            id="monthdata-selector"
+                        >
+                            <option
+                                selected
+                                v-for="monthdata in monthdatas"
+                                :key="monthdata.id"
+                                :value="monthdata.id"
+                            >
+                                {{ monthdata.month }}
+                            </option>
+                        </select>
+
+                        <!-- <DetailsAddHutang />
+                        <DetailsSubmitPayment @submitPayment="submitPayment" /> -->
+                        <DetailRecentSales :account="account!" />
+                    </div>
+                </div>
+            </template>
+        </div>
+    </CompositeNav>
 </template>
 
-<style scoped>
-.container-main {
-    border-color: rgb(83, 83, 83) !important;
-    border-radius: 15px;
-}
-</style>
+<style scoped></style>
