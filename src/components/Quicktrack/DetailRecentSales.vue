364a3c551e4d27513644efbf168d01417587e17a
anthocodes99 <anthocodes99@users.noreply.github.com>
2023-01-26 10:24:11 +0800

    refactor(frontend): :art: add parent div to DetailRecentSales
    to allow for margin adjustment without going inside the component

--- src/components/Quicktrack/DetailRecentSales.vue
+++ src/components/Quicktrack/DetailRecentSales.vue
@@ -33,37 +33,42 @@ const sortedSales = sortSales(props.account.recent_sales)
 </script>
 
 <template>
-    <h2 class="text-2xl pt-8">Recent Sales</h2>
-    <ul class="flex-auto flex-col items-center">
-        <li v-for="[day, sales] of Object.entries(sortedSales)" :key="day">
-            <h4 class="text-lg text-center text-white">{{ day }}</h4>
-            <template v-for="sale in sales">
-                <!-- <RuncitSaleItem v-bind="sale" /> -->
-                <li
-                    class="flex items-center justify-between my-2 py-1 border-b"
-                >
-                    <div>
-                        <h3 class="text-md text-gray-200">
-                            {{ sale.product }}
-                        </h3>
-                        <span class="text-sm text-gray-500"
-                            >{{ sale.quantity }} pcs @ RM
-                            <!-- + in front of unit_price will convert it into number -->
-                            <!-- unit_price is a string -->
-                            {{ (+sale.unit_price).toFixed(2) }} ea</span
-                        >
-                    </div>
-                    <div>
-                        <h3 class="text-md text-gray-200">
-                            RM
-                            {{ (+sale.unit_price * sale.quantity).toFixed(2) }}
-                        </h3>
-                    </div>
-                </li>
-            </template>
-        </li>
-        image.png
-    </ul>
+    <div>
+        <h2 class="text-2xl">Recent Sales</h2>
+        <ul class="flex-auto flex-col items-center">
+            <li v-for="[day, sales] of Object.entries(sortedSales)" :key="day">
+                <h4 class="text-lg text-center text-white">{{ day }}</h4>
+                <template v-for="sale in sales">
+                    <!-- <RuncitSaleItem v-bind="sale" /> -->
+                    <li
+                        class="flex items-center justify-between my-2 py-1 border-b"
+                    >
+                        <div>
+                            <h3 class="text-md text-gray-200">
+                                {{ sale.product }}
+                            </h3>
+                            <span class="text-sm text-gray-500"
+                                >{{ sale.quantity }} pcs @ RM
+                                <!-- + in front of unit_price will convert it into number -->
+                                <!-- unit_price is a string -->
+                                {{ (+sale.unit_price).toFixed(2) }} ea</span
+                            >
+                        </div>
+                        <div>
+                            <h3 class="text-md text-gray-200">
+                                RM
+                                {{
+                                    (+sale.unit_price * sale.quantity).toFixed(
+                                        2
+                                    )
+                                }}
+                            </h3>
+                        </div>
+                    </li>
+                </template>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <style scoped></style>
