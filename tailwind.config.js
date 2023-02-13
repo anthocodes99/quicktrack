65e0cd6cb7d1e4ad6a9fcd1348e7be2c11d0e463
anthocodes99 <anthocodes99@users.noreply.github.com>
2023-02-13 10:11:34 +0800

    refactor(frontend): :art: Added new color-scheme dark
    dark-900 to dark-100

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
     content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                dark: {
+                    100: '#555E62',
+                    200: '#4C5457',
+                    300: '#42494C',
+                    400: '#393F41',
+                    500: '#2F3437',
+                    600: '#252A2C',
+                    700: '#1C1F21',
+                    800: '#131516',
+                    900: '#090A0B',
+                },
+            },
+        },
     },
     plugins: [],
 }
