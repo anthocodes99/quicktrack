c0aaf44b494e2f4c85888c040f815f5bbcf78e70
anthocodes99 <anthocodes99@users.noreply.github.com>
2023-02-13 10:14:23 +0800

    feat(frontend): :sparkles: Added Create Expense Form
    Icons WIP at the moment.

--- src/views/Runcitworks/RuncitworksExpenses.vue
+++ src/views/Runcitworks/RuncitworksExpenses.vue
@@ -5,6 +5,8 @@ import { useMonthdataStore } from '../../store/monthdata'
 
 import { Monthdata } from '../../models/monthdata'
 import { useTransactionParser } from '../../composables/transactionParser'
+import BaseInput from '../../primitives/BaseInput.vue'
+import BaseButton from '../../primitives/BaseButton.vue'
 
 interface Props {
     currentMonthdata: Monthdata
@@ -18,6 +20,8 @@ const runcitPurchase = {
 }
 
 const props = defineProps<Props>()
+
+const revealAddForm = ref(false)
 
 const currentMonthdata = toRef(props, 'currentMonthdata')
 
@@ -52,12 +56,58 @@ onMounted(() => {
 
 <template>
     <div>
-        <h2 class="text-4xl">Expenses</h2>
+        <div class="flex justify-between">
+            <h2 class="text-4xl pb-4">Expenses</h2>
+            <div class="flex gap-2">
+                <BaseButton
+                    class="w-12 h-12"
+                    @click="revealAddForm = !revealAddForm"
+                    >add</BaseButton
+                >
+                <BaseButton class="w-12 h-12">del</BaseButton>
+            </div>
+        </div>
+        <form
+            class="bg-dark-700 p-4 rounded-lg border border-dark-400"
+            v-if="revealAddForm"
+            @submit.prevent=""
+        >
+            <div class="">
+                <label class="text-gray-400 text-lg" for="date">Date</label>
+                <BaseInput type="date" id="date" class="h-10 w-48" />
+            </div>
+            <div class="mt-4">
+                <label class="text-gray-400 text-lg" for="description"
+                    >Description</label
+                >
+                <BaseInput type="text" id="description" class="h-10 w-48" />
+            </div>
+            <div class="mt-4">
+                <label class="text-gray-400 text-lg" for="unit_price"
+                    >Unit Price</label
+                >
+                <BaseInput type="number" id="unit_price" class="h-10 w-48" />
+            </div>
+            <div class="mt-4">
+                <label class="text-gray-400 text-lg" for="quantity"
+                    >Quantity</label
+                >
+                <BaseInput
+                    type="number"
+                    step="1"
+                    id="quantity"
+                    class="h-10 w-48"
+                />
+            </div>
+            <div class="mt-4">
+                <BaseButton>Create Expense</BaseButton>
+            </div>
+        </form>
         <template v-if="expensesList.length == 0">
             <h3 class="text-2xl text-white text-center mt-12">No expenses~</h3>
         </template>
         <template v-else>
-            <RuncitExpenseList :expensesList="expensesList" />
+            <RuncitExpenseList class="mt-8" :expensesList="expensesList" />
         </template>
     </div>
 </template>
